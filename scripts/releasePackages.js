b6746e256606d38941ac0d05021c247bf24558de
William C. Johnson <wcjohnson@oigroup.net>
2017-06-20 15:17:33 -0400

    Improve release script

--- scripts/releasePackages.js
+++ scripts/releasePackages.js
@@ -1,5 +1,6 @@
 const { run, runArgs, lernaScopedCommand, lernaScopedCapture } = require('./run')
 const path = require('path')
+const inquirer = require('inquirer')
 
 // check that we're not running through `yarn`,
 // which breaks because `npm whoami` returns nil
@@ -10,51 +11,73 @@ try {
   process.exit()
 }
 
-const packages = process.argv.slice(2)
-
-const packageDirs = packages.map(package => {
-  const abs = lernaScopedCapture([package], 'exec -- pwd').trim()
-  return path.relative(path.join(__dirname, '..'), abs)
+const packageList = process.argv.slice(2)
+const packages = packageList.map(packageName => {
+  // Absolute path
+  const abs = lernaScopedCapture([packageName], 'exec -- pwd').trim()
+  // git status
+  const gitStatus = lernaScopedCapture([packageName], 'exec -- git status --porcelain').trim()
+  // git branch
+  const gitBranch = lernaScopedCapture([packageName], 'exec -- git rev-parse --abbrev-ref HEAD').trim()
+  return {
+    name: packageName,
+    path: path.relative(path.join(__dirname, '..'), abs),
+    gitStatus: gitStatus,
+    gitBranch: gitBranch
+  }
 })
-console.log(packageDirs)
 
-// health check
+const packageDirs = packages.map(package => package.path)
+
+/////////////////// Health check
+console.log("Preflight check:")
 packages.forEach(package => {
-  const status = lernaScopedCapture([package], 'exec -- git status --porcelain').trim()
-  if (status.length > 0) {
-    throw new Error(`Package ${package} has a dirty repository.`)
+  console.log("----------------------------------")
+  console.log("Package: ", package.name)
+  console.log("Path: ", package.path)
+  console.log("Branch: ", package.gitBranch)
+  console.log("----------------------------------")
+  if (package.gitStatus.length > 0) {
+    throw new Error(`Package ${package.name} has a dirty repository.`)
+  }
+  if (package.gitBranch === "HEAD") {
+    throw new Error(`Package ${package.name} has a detached HEAD.`)
   }
 });
 
-lernaScopedCommand(packages, `exec -- git pull`)
-lernaScopedCommand(packages, `run preversion`)
+inquirer.prompt([{type: 'confirm', default: false, name: 'go', message: 'Proceed?'}])
+.then( answers => {
+  if (!answers.go) throw new Error("User aborted release")
 
-// allow user intervention on version numbers for each package
-lernaScopedCommand(packages, `publish --skip-npm --skip-git --exact`)
+  lernaScopedCommand(packageList, `run preversion`)
 
-// read version numbers and push git tags
-const versionedPackages = packages.map(package => ({
-  version: lernaScopedCapture([package], 'exec -- cat package.json | sed -nE "s/.*\\"version\\": ?\\"(.*)\\".*/\\1/p"').trim(),
-  package: package
-}))
-console.log(versionedPackages)
+  //////////////////////// Version bump
+  // allow user intervention on version numbers for each package
+  lernaScopedCommand(packageList, `publish --skip-npm --skip-git --exact`)
 
-versionedPackages.forEach(info => {
-  lernaScopedCommand([info.package], `exec -- git commit -am v${info.version}`)
-  lernaScopedCommand([info.package], `exec -- git tag v${info.version} -m v${info.version}`)
+  // read version numbers and push git tags
+  packages.forEach(package => {
+    package.version = lernaScopedCapture([package.name], 'exec -- cat package.json | sed -nE "s/.*\\"version\\": ?\\"(.*)\\".*/\\1/p"').trim()
+    lernaScopedCommand([package.name], `exec -- git commit -am v${package.version}`)
+    lernaScopedCommand([package.name], `exec -- git tag v${package.version} -m v${package.version}`)
+  })
+
+  //////////////////////// Publish and push tags
+  lernaScopedCommand(packageList, `exec -- npm publish`)
+  lernaScopedCommand(packageList, `exec -- git push && git push --tags`)
+
+  //////////////////////// Push monorepo updates
+  run(`git add ${packageDirs.join(' ')}`)
+
+  commitArgs = ['commit', '-m', 'Publish', '-m', 'Publish to NPM:']
+  packages.forEach(info => {
+    commitArgs.push('-m', `* ${info.name} v${info.version}`)
+  })
+  runArgs('git', commitArgs)
+
+  run(`git push`)
+} )
+.catch(err => {
+  console.error(err)
+  process.exit(1)
 })
-
-lernaScopedCommand(packages, `exec -- npm publish`)
-
-lernaScopedCommand(packages, `exec -- git push && git push --tags`)
-
-// commit & push version bump at monorepo level
-run(`git add ${packageDirs.join(' ')}`)
-
-commitArgs = ['commit', '-m', 'Publish', '-m', 'Publish to NPM:']
-versionedPackages.forEach(info => {
-  commitArgs.push('-m', `* ${info.package} v${info.version}`)
-})
-runArgs('git', commitArgs)
-
-run(`git push`)
