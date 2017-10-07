f26ae526a267ce3d1a9929a7af5bd27a0ecb7083
William C. Johnson <wcjohnson@oigroup.net>
2017-10-06 21:34:51 -0400

    Fix self linking

--- scripts/selfLink.js
+++ scripts/selfLink.js
@@ -3,7 +3,7 @@
 const run = require('./run')
 
 process.argv.slice(2).forEach(target => {
-  run(`mkdirp ${target}/node_modules`)
+  run(`mkdir -p ${target}/node_modules`)
   run(`touch ${target}/node_modules/${target}`)
   run(`rm ${target}/node_modules/${target}`)
   run(`ln -s ../../${target} ${target}/node_modules/${target}`)
