f264137c40f0e8796bdb87a0517ee077e4e69743
William Johnson <wcjohnson@oigroup.net>
2018-09-22 13:54:40 -0700

    build: MIsc fixes

--- scripts/selfLink.js
+++ scripts/selfLink.js
@@ -6,3 +6,8 @@ run(`mkdir -p lightscript-eslint/node_modules/@lightscript`)
 run(`touch lightscript-eslint/node_modules/@lightscript/eslint-plugin`)
 run(`rm lightscript-eslint/node_modules/@lightscript/eslint-plugin`)
 run(`ln -s ../.. lightscript-eslint/node_modules/@lightscript/eslint-plugin`)
+
+run(`mkdir -p lightscript-eslint/node_modules/@lightscript`)
+run(`touch lightscript-eslint/node_modules/@lightscript/babel-preset`)
+run(`rm lightscript-eslint/node_modules/@lightscript/babel-preset`)
+run(`ln -s ../../../lightscript-preset lightscript-eslint/node_modules/@lightscript/babel-preset`)
